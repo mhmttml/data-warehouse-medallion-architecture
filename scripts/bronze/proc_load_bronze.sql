554bb95da4f8ed9cf2bb779bfffda10520305cad
Mehmet TEMEL <119588589+mhmttml@users.noreply.github.com>
2025-04-29 14:10:13 +0300

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,5 +1,5 @@
 
-BULK INSERT INTO TABLE crm_cust_info
+BULK INSERT crm_cust_info
 FROM '~/datasets/cust_info.csv'
 WITH (
 	FIRSTROW = 2,
@@ -8,7 +8,7 @@ WITH (
 );
 
 
-BULK INSERT INTO TABLE crm_sales_details
+BULK INSERT crm_sales_details
 FROM '~/datasets/sales_details.csv'
 WITH (
 	FIRSTROW = 2,
@@ -17,7 +17,7 @@ WITH (
 );
 
 
-BULK INSERT INTO TABLE crm_prd_info
+BULK INSERT crm_prd_info
 FROM '~/datasets/prd_info.csv'
 WITH (
 	FIRSTROW = 2,
@@ -26,7 +26,7 @@ WITH (
 );
 
 
-BULK INSERT INTO TABLE erp_cust_az12 
+BULK INSERT erp_cust_az12 
 FROM '~/datasets/erp_cust_az12.csv'
 WITH (
 	FIRSTROW = 2,
@@ -35,7 +35,7 @@ WITH (
 );
 
 
-BULK INSERT INTO TABLE erp_loc_a101 
+BULK INSERT erp_loc_a101 
 FROM '~/datasets/loc_a101.csv'
 WITH (
 	FIRSTROW = 2,
@@ -44,7 +44,7 @@ WITH (
 );
 
 
-BULK INSERT INTO TABLE erp_px_cat_g1v2 
+BULK INSERT erp_px_cat_g1v2 
 FROM '~/datasets/px_cat_g1v2.csv'
 WITH (
 	FIRSTROW = 2,
